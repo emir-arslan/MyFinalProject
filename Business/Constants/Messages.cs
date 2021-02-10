4caab5e3bd117ab16e994d84e0b51c1ddb1820c4
arsla <arsla@EMIR-GIZEM>
2021-02-11 00:04:32 +0300

    10 Şubat Ders Sonu.

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,16 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Constants
+{
+    //new lememek için static verdik
+    public static class Messages
+    {
+        public static string ProductAdded = "Ürün Eklendi";
+        public static string ProductNameInvalid = "Ürün ismi geçersiz.";
+        internal static string MaintenanceTime = "Sistem bakımdadır.";
+        internal static string ProductsListed = "Ürünler listelendi";
+    }
+}
---
+++ Core/Utilities/Results/Result.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+    public class Result : IResult
+    {
+
+        public Result(bool success, string message):this(success) //2 parametrelik constructer çalışacağı zaman ikisi birden çalışması için
+        {
+            Message = message;
+        }
+
+        public Result(bool success)
+        {
+            Success = success;
+        }
+
+        public bool Success { get; }
+
+        public string Message { get; }
+    }
+}
